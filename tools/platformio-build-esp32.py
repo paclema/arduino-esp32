d423176281873432e0657832eae0c412c13f83f5
Pablo Clemente <paclema@gmail.com>
2023-11-08 16:42:39 +0100

    Update Arduino Release v2.0.14 with PSRAM_mbedTLS_LWIP esp32-arduino-lib-builder

--- tools/platformio-build-esp32.py
+++ tools/platformio-build-esp32.py
@@ -328,7 +328,7 @@ env.Append(
         "UNITY_INCLUDE_CONFIG_H",
         "WITH_POSIX",
         "_GNU_SOURCE",
-        ("IDF_VER", '\\"v4.4.6-dirty\\"'),
+        ("IDF_VER", '\\"v4.4.6\\"'),
         "ESP_PLATFORM",
         "_POSIX_READER_WRITER_LOCKS",
         "ARDUINO_ARCH_ESP32",
